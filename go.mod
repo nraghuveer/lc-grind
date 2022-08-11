5d9c91323a4b640078b48c4bf32681f86b71411b
Raghuveer N <narahar2@uwm.edu>
2022-08-11 08:56:32 +0530

    initial commit

---
+++ go.mod
@@ -0,0 +1,32 @@
+module lc-grind/m
+
+go 1.18
+
+require (
+	github.com/charmbracelet/bubbles v0.13.0
+	github.com/charmbracelet/bubbletea v0.22.0
+	github.com/charmbracelet/lipgloss v0.5.0
+)
+
+require (
+	git.mills.io/prologic/bitcask v1.0.2 // indirect
+	github.com/abcum/lcp v0.0.0-20201209214815-7a3f3840be81 // indirect
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/containerd/console v1.0.3 // indirect
+	github.com/gofrs/flock v0.8.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
+	github.com/muesli/cancelreader v0.2.1 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.11.1-0.20220212125758-44cd13922739 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/plar/go-adaptive-radix-tree v1.0.4 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/sahilm/fuzzy v0.1.0 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	golang.org/x/exp v0.0.0-20200228211341-fcea875c7e85 // indirect
+	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
+)
---
+++ main.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/bubbles/spinner"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+var docStyle = lipgloss.NewStyle().Margin(1, 2)
+
+func (i submission) Title() string       { return i.ProblemTitle }
+func (i submission) Description() string { return i.Time + " Ago" }
+func (i submission) FilterValue() string { return i.Title() }
+
+type submissionsLoadCmd struct{ items []submission }
+
+type model struct {
+	list          list.Model
+	spinner       spinner.Model
+	isLoadingData bool
+	note          string
+	loadedNotes   map[string]string
+}
+
+func InitModel() model {
+	return model{list: list.New([]list.Item{}, list.NewDefaultDelegate(), 0, 0), spinner: spinner.New(), isLoadingData: true, note: "", loadedNotes: make(map[string]string)}
+}
+
+func loadSubmissionsCmd() tea.Cmd {
+	return func() tea.Msg {
+		submissions, _ := GetAllSubmissions(time.Date(2022, time.May, 1, 0, 0, 0, 0, time.UTC))
+		return submissionsLoadCmd{items: submissions}
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return tea.Batch(spinner.Tick, loadSubmissionsCmd())
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch keypress := msg.String(); keypress {
+		case "ctrl+c":
+			return m, tea.Quit
+		case "up", "k", "down", "j":
+			var cmd tea.Cmd
+			m.list, cmd = m.list.Update(msg)
+
+			curQuestion, ok := m.list.Items()[m.list.Index()].(submission)
+			note, noteOk := m.loadedNotes[curQuestion.Title_slug]
+			if ok && noteOk {
+				m.note = note
+			} else {
+				m.note = ""
+			}
+			return m, cmd
+		case "enter":
+			curQuestion, ok := m.list.Items()[m.list.Index()].(submission)
+			if ok {
+				note, ok := m.loadedNotes[curQuestion.Title_slug]
+				if !ok {
+					note = getNote(curQuestion.Title_slug)
+					m.loadedNotes[curQuestion.Title_slug] = note
+				}
+				m.note = note
+			}
+		}
+	case tea.WindowSizeMsg:
+		h, v := docStyle.GetFrameSize()
+		m.list.SetSize(msg.Width-h, msg.Height-v)
+	case submissionsLoadCmd:
+		var items []list.Item
+		for _, sub := range msg.items {
+			items = append(items, sub)
+		}
+		m.list.SetItems(items)
+		m.isLoadingData = false
+	case spinner.TickMsg:
+		var cmd tea.Cmd
+		m.spinner, cmd = m.spinner.Update(msg)
+		return m, cmd
+	}
+
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	var views []string
+	if m.isLoadingData {
+		views = append(views, docStyle.Render(fmt.Sprintf("\n\n   %s Loading...\n\n", m.spinner.View())))
+	} else {
+		views = append(views, docStyle.Render(m.list.View()))
+	}
+	note := lipgloss.NewStyle().
+		Width(75).
+		Height(m.list.Height()).
+		Padding(2).Border(lipgloss.ThickBorder(), false, false, false, true).
+		BorderBackground(lipgloss.Color("63")).
+		Render(m.note)
+	block := lipgloss.PlaceHorizontal(80, lipgloss.Left, note)
+	views = append(views, block)
+	return lipgloss.JoinHorizontal(lipgloss.Top, views...)
+}
+
+func main() {
+	db, err := GetDB()
+	if err != nil {
+		log.Fatalln("Failed to create db instance", err.Error())
+	}
+	defer db.Close()
+	m := InitModel()
+	m.spinner.Spinner = spinner.Dot
+	m.spinner.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+	m.list.Title = "Latest Submissions"
+
+	p := tea.NewProgram(m, tea.WithAltScreen())
+
+	if err := p.Start(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
+}
